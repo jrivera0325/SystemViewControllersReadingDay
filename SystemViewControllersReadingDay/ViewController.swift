e61ba54f98888ff41639282f27157dbee8f4a35f
Julio Rivera <jrivera0325@jh-mba158-16.local>
2018-11-29 11:41:45 -0600

    pg.672

--- SystemViewControllersReadingDay/ViewController.swift
+++ SystemViewControllersReadingDay/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
     }
 
-
+    @IBAction func shareButtonTapped(_ sender: UIButton) {
+        guard let image = imageView.image else { return }
+        let activityController = UIActivityViewController(activityItems:[image], applicationActivities: nil)
+        activityController.popoverPresentationController?.sourceView = sender
+        
+        present(activityController, animated: true, completion: nil)
+    }
+    
+    @IBAction func safariButtonTapped(_ sender: UIButton) {
+        if let url = URL(string: "http://www.apple.com") {
+            let safariViewContoller = SFSafariViewController(url: url); present(safariViewContoller, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func photosButtonTapped(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "Chose Image Source", message: nil, preferredStyle: .actionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil); alertController.addAction(cancelAction); alertController.popoverPresentationController?.sourceView = sender
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func emailButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+    
+    
 }
 
