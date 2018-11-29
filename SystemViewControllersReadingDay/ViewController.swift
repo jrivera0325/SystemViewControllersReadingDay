7635044814a2b669338e80c2af177292d4a3b983
Julio Rivera <jrivera0325@jh-mba158-16.local>
2018-11-29 11:01:19 -0600

    Initial Commit

---
+++ SystemViewControllersReadingDay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SystemViewControllersReadingDay
+//
+//  Created by Julio Rivera on 11/29/18.
+//  Copyright © 2018 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
